17cea2386d5f0ea4038378b6f5ce2cb17c57f3f1
Jeff Ferber <jeff@proxv.com>
2012-05-01 20:36:06 -0700

    fix homepage for rebuild script

--- script/rebuild.rb
+++ script/rebuild.rb
@@ -8,10 +8,12 @@ UrlPrefix = "http://127.0.0.1:#{ARGV[0] || '8080'}"
 PublicDir = "#{ArrayDir}/public"
 
 StaticPages::Pages.each do |page_name|
+  page_name = page_name.to_s
   #expire_page( :controller => 'static', :action => page_name )
   html = open("#{UrlPrefix}/#{page_name}").read
   #html = exec("curl #{UrlPrefix}/#{page_name}")
-  dir = "#{PublicDir}/#{page_name}"
+  dir = "#{PublicDir}/#{page_name == 'home' ? '' : page_name}"
+  puts "tmp ---- #{page_name} dir: #{dir}"
   FileUtils.mkdir_p(dir)
   File.open("#{dir}/index.html", 'w') {|f| f.write(html); f.close }
   puts "Page #{page_name} length: #{html.length}"
